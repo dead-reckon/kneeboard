9170aea977524add9152881e3c9b34bcb3ab3fe4
JB <jb@unitesystem.com>
2019-01-20 08:50:52 -0600

    adding models

--- board/il2/models.py
+++ board/il2/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Meta(models.Model):
-	pass
+	
+	plane = models.CharField('Name of Plane', max_length=140)
+	takeoff = models.CharField('Takeoff Speed', max_length=140)
+	glideslope = models.CharField('Glideslope Speed', max_length=140)
+	landing = models.CharField('Landing Speed', max_length=140)
+	dive = models.CharField('Dive Speed Limit', max_length=140)
 
 	def __str__(self):
-        return self.op_name
+        return self.plane
